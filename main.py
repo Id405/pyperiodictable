3c7af729ad21312acb03cb8379b8a6257ce2cf12
U-FSD\s151542 <s151542@WVH-144-84724.fsd.domain>
2019-11-26 11:00:35 -0900

    Begin adding detail to elements when clicked

--- main.py
+++ main.py
@@ -8,37 +8,41 @@ class PeriodicTable(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Hello World")
         
-        grid = Gtk.Grid()
-        self.add(grid)
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
         
-        elements = []
+        grid = Gtk.Grid()
+        self.notebook.append_page(grid, Gtk.Label(label='Periodic Table'))
+        
+        self.elements = []
+        self.pagenumbers = {}
         
         with open('PTable.csv', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                elements.append(row)
+                self.elements.append(row)
         
         
-        for element in elements:
+        for element in self.elements:
             if(element['Group'].isdigit()):
                 # button = Gtk.Button(label='')
                 # grid.attach(button, int(element['Group']), int(element['Period']), 1, 1)
                 
                 box = Gtk.VBox()
                 
-                number = Gtk.Label(element['AtomicNumber'])
-                name = Gtk.Label(element['Element'])
-                symbol = Gtk.Label(element['Symbol'])
-                mass = Gtk.Label(element['AtomicMass'])
+                number = Gtk.Label(label=element['AtomicNumber'])
+                name = Gtk.Label(label=element['Element'])
+                symbol = Gtk.Label(label=element['Symbol'])
+                mass = Gtk.Label(label=element['AtomicMass'])
                 
                 box.add(number)
                 box.add(symbol)
                 box.add(name)
                 box.add(mass)
                 
-                button = Gtk.Button(label = None)
+                button = Gtk.ToggleButton()
                 button.add(box)
-                button.connect("clicked", self.on_Element_Clicked)
+                button.connect("toggled", self.on_element_toggled, int(element['AtomicNumber']))
                 
                 grid.attach(button, int(element['Group']), int(element['Period']), 1, 1)
 
@@ -48,8 +52,14 @@ class PeriodicTable(Gtk.Window):
         grid.set_row_spacing(3)
         grid.set_column_spacing(3)
     
-    def on_Element_Clicked(self, widget):
-        print(widget)
+    def on_element_toggled(self, widget, number):
+        if widget.get_active():
+            box = Gtk.VBox()
+            box.add(Gtk.Label(label="test"))
+            self.notebook.append_page(box, Gtk.Label(label=self.elements[number]['Symbol']))
+        else:
+            self.notebook.remove(pagenumbers[number]
+        self.show_all()
 
 win = PeriodicTable()
 win.connect("destroy", Gtk.main_quit)
