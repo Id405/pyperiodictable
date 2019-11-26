0c03b51851b2bd49a393fdaa6f7aa5800ff9a4c0
U-FSD\s151542 <s151542@WVH-144-84724.fsd.domain>
2019-11-26 10:22:29 -0900

    Load data from csv, add more info to buttons

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
+import csv
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class Element():
-	def __init__(self, s, n, nu, m):
-		self.symbol = s
-		self.name = n
-		self.number = nu
-		self.mass = m
-
-elements = [
-            [Element('H', 'Hydrogen', '1', '1.008'), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Element('He', 'Helium', '2', '4.0026')], 
-            [Element('Li', 'Lithium', '3', '6.94'), Element('Be', 'Beryllium', '4', '9.0122'), None, None, None, None, None, None, None, None, None, None, Element('B', 'Boron', '5', '10.81'), Element('C', 'Carbon', '6', '12.011'), Element('N', 'Nitrogen', '7', '14.007'), Element('O', 'Oxygen', '8', '15.99'), Element('F', 'Flourine', '9', '18.998'), Element('Ne', 'Helium', '2', '4.0026')],
-            [Element('H', 'Hydrogen', '1', '1.008'), None, None, None, None, None, None, None, None, None, None, None, Element('Al', 'Aluminium', '13', '26.982'), Element('Si', 'Silicone', '14', '28.085'), Element('P', 'Phosphorus', '15', '30.974'), Element('O', 'Oxygen', '8', '15.99'), Element('F', 'Flourine', '9', '18.998'), Element('Ne', 'Helium', '2', '4.0026')],
-            [Element('H', 'Hydrogen', '1', '1.008'), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Element('He', 'Helium', '2', '4.0026')],
-            [Element('H', 'Hydrogen', '1', '1.008'), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Element('He', 'Helium', '2', '4.0026')],
-            [Element('H', 'Hydrogen', '1', '1.008'), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Element('He', 'Helium', '2', '4.0026')],
-            [Element('H', 'Hydrogen', '1', '1.008'), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Element('He', 'Helium', '2', '4.0026')],
-            [Element('H', 'Hydrogen', '1', '1.008'), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Element('He', 'Helium', '2', '4.0026')],
-            [Element('H', 'Hydrogen', '1', '1.008'), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Element('He', 'Helium', '2', '4.0026')],
-           ]
-
 class PeriodicTable(Gtk.Window):
-	def __init__(self):
-		Gtk.Window.__init__(self, title="Hello World")
-
-		grid = Gtk.Grid()
-		self.add(grid)
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello World")
         
-		for y, row in enumerate(elements):
-			for x, element in enumerate(row):
-				if element != None:
-					button = Gtk.Button(label=element.symbol)
-					grid.attach(button, x, y, 1, 1)
-				else:
-					label = Gtk.Label("")
-					grid.attach(label, x, y, 1, 1)
-		
-		grid.set_row_homogeneous(True)
-		grid.set_column_homogeneous(True)
-		
-	def on_button_clicked(self, widget):
-		print("Hello World")
+        grid = Gtk.Grid()
+        self.add(grid)
+        
+        elements = []
+        
+        with open('PTable.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                elements.append(row)
+        
+        
+        for element in elements:
+            if(element['Group'].isdigit()):
+                # button = Gtk.Button(label='')
+                # grid.attach(button, int(element['Group']), int(element['Period']), 1, 1)
+                
+                box = Gtk.VBox()
+                
+                number = Gtk.Label(element['AtomicNumber'])
+                name = Gtk.Label(element['Element'])
+                symbol = Gtk.Label(element['Symbol'])
+                mass = Gtk.Label(element['AtomicMass'])
+                
+                box.add(number)
+                box.add(symbol)
+                box.add(name)
+                box.add(mass)
+                
+                button = Gtk.Button(label = None)
+                button.add(box)
+                button.connect("clicked", self.on_Element_Clicked)
+                
+                grid.attach(button, int(element['Group']), int(element['Period']), 1, 1)
+
+        
+        grid.set_row_homogeneous(True)
+        grid.set_column_homogeneous(True)
+        grid.set_row_spacing(3)
+        grid.set_column_spacing(3)
+    
+    def on_Element_Clicked(self, widget):
+        print(widget)
 
 win = PeriodicTable()
 win.connect("destroy", Gtk.main_quit)
